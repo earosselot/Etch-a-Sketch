c743b49511bfc20854e9e582351f38539aeadbab
earosselot <earosselot@gmail.com>
2021-01-25 03:03:09 -0300

    changed from flexbox to grid. Board working. Lacks some improvements.

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
-const container = document.querySelector('#container');
 
 
-
-for (let i=1; i<10; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    container.appendChild(row);
-    for (let j=1; j<10; j++){
-        const item = document.createElement('div');
-        item.classList.add('item');
-        // item.setAttribute('flex', '1 0 24%');
-        // flex-grow, flex-shrink, flex-basis
-        item.setAttribute('width', '10%');
-
-
-        item.textContent = 'div';
-        row.appendChild(item);
-    }
+function allowPaint(gridItems) {
+    gridItems.forEach(grid_item => {
+        grid_item.addEventListener('mouseover', mouseOver);
+        grid_item.addEventListener('mouseout', mouseOut);
+    })
 }
+
+function createGrid(grid_size=32) {
+
+    container.style['grid-template-columns'] = `repeat(${grid_size}, auto)`;
+    container.style['grid-template-rows'] = `repeat(${grid_size}, auto)`;
+
+    for (let i=1; i<=(grid_size ** 2); i++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('gridItem');
+        container.appendChild(gridItem);
+    }
+
+    let gridItems = document.querySelectorAll('.gridItem');
+    allowPaint(gridItems);
+}
+
+
+
+function mouseOver(e) {
+    e.target.classList.add('mouseOver');
+}
+
+function mouseOut(e) {
+    e.target.classList.add('mouseOut');
+}
+
+function resetGrid() {
+    gridSize = parseInt(prompt('grid size (max 100): '));
+    // FALTAN COMPROBACIONES Y LIMITES PARA EL PROMPT
+    container.innerHTML = '';
+    createGrid(gridSize);
+}
+
+
+const container = document.querySelector('#container');
+createGrid();
+
+const resetBtn = document.querySelector('#reset');
+resetBtn.addEventListener('click', resetGrid);
+
+// FALTA AGREGAR LOS OPCIONALES
